518a33f9a1ca0c7adf235f1dce3f3d6f8538b7fa
CH <gigamegs@gmail.com>
2022-07-28 16:48:35 +0200

    database layer refactoring

---
+++ app/date_and_data.py
@@ -0,0 +1,28 @@
+import re, datetime
+
+def normalize_date(date):
+  try: 
+    datetime_object = datetime.datetime.strptime(date, '%B %d %Y')           
+  except:
+    try:
+      datetime_object = datetime.datetime.strptime(date, '%d %b %Y')
+    except: 
+       match = re.search("\d{4}-\d{2}-\d{2}", date)
+       if match is None:
+        print ("Not a valid date: data dropped!")
+        date = -1
+       if match is not None:
+        print("Date confirmed: "+date)
+    else: 
+      date = datetime_object.strftime("%Y-%m-%d")
+      print("Date confirmed: "+date)
+  else:
+   date = datetime_object.strftime("%Y-%m-%d")
+   print("Date confirmed: "+date)              
+  return date
+
+def scrub_input(data):
+  if (data == "null" or data == ""):
+    print("Not a valid data: data dropped!")
+    data = -1
+  return data
--- app/server.py
+++ app/server.py
@@ -1,45 +1,8 @@
-import pymysql.cursors, re, datetime, os
 from flask import Flask, render_template, jsonify, make_response, request, abort
+from database import *
+from date_and_data import *
+
 server = Flask(__name__)
-bookmarks = []
-
-def normalize_date(date):
-  try: 
-    datetime_object = datetime.datetime.strptime(date, '%B %d %Y')           
-  except:
-    try:
-      datetime_object = datetime.datetime.strptime(date, '%d %b %Y')
-    except: 
-       match = re.search("\d{4}-\d{2}-\d{2}", date)
-       if match is None:
-        print ("Not a valid date: data dropped!")
-        date = -1
-       if match is not None:
-        print("Date confirmed: "+date)
-    else: 
-      date = datetime_object.strftime("%Y-%m-%d")
-      print("Date confirmed: "+date)
-  else:
-   date = datetime_object.strftime("%Y-%m-%d")
-   print("Date confirmed: "+date)              
-  return date
-
-def scrub_input(data):
-  if (data == "null" or data == ""):
-    print("Not a valid data: data dropped!")
-    data = -1
-  return data
-    
-def get_db_connection():
-  try:
-    connection = pymysql.connect(host=os.environ.get('MYSQL_HOST'),
-                                 user=os.environ.get('MYSQL_USER'),
-                                 password=os.environ.get('MYSQL_PWD'),
-                                 database=os.environ.get('MYSQL_DB'))
-  except: 
-    print("MySQL connection error!")
-  else:
-    return connection
       
 @server.route("/")
 def index():
@@ -47,54 +10,34 @@ def index():
 
 @server.route('/avaaz/api/v1.0/bookmarks', methods=['GET'])
 def get_bookmarks():
-    connection = get_db_connection()
-    query = "SELECT * from data"
-    cursor = connection.cursor()
-    cursor.execute(query)
-    bookmarks=cursor.fetchall()
-    cursor.close()
+    db = Database()
+    bookmarks = db.fetchall("SELECT * from data", "")
     if len(bookmarks) == 0:
-        abort(400)
-    connection.close()            
+        abort(400)         
     return jsonify({'bookmarks': bookmarks}), 201
 
 @server.route('/avaaz/api/v1.0/bookmarks/<int:bookmark_id>', methods=['GET'])
 def get_id(bookmark_id):
-    connection = get_db_connection()
-    query = "SELECT * from data WHERE id = %s"
-    params = (bookmark_id)
-    cursor = connection.cursor()
-    cursor.execute(query, params)
-    bookmark=cursor.fetchone()
+    db = Database()
+    bookmark = db.fetchone("SELECT * from data WHERE id = %s", bookmark_id)
     if len(bookmark) == 0:
         abort(400)
-    connection.close()
     return jsonify({'bookmark': bookmark}), 201
 
 @server.route('/avaaz/api/v1.0/bookmarks/title/<string:bookmark_title>', methods=['GET'])
 def get_title(bookmark_title):
-    connection = get_db_connection()
-    query = "SELECT * from data where title Like %s"
-    params = "%"+bookmark_title+"%"
-    cursor = connection.cursor()
-    cursor.execute(query, (params))
-    bookmarks=cursor.fetchall()
+    db = Database()
+    bookmarks=db.fetchall("SELECT * from data where title Like %s", ("%"+bookmark_title+"%"))
     if len(bookmarks) == 0:
         abort(400)
-    connection.close()
     return jsonify({'bookmarks': bookmarks}), 201
       
 @server.route('/avaaz/api/v1.0/bookmarks/url/<string:bookmark_url>', methods=['GET'])
 def get_url(bookmark_url):
-    connection = get_db_connection()
-    query = "SELECT * from data where REPLACE(REPLACE(url,'http://',''),'https://','') Like %s"
-    params = "%"+bookmark_url+"%"
-    cursor = connection.cursor()
-    cursor.execute(query, (params))
-    bookmarks=cursor.fetchall()
+    db = Database()
+    bookmarks=db.fetchall("SELECT * from data where REPLACE(REPLACE(url,'http://',''),'https://','') Like %s", ("%"+bookmark_url+"%"))
     if len(bookmarks) == 0:
-        abort(400)
-    connection.close()    
+        abort(400)   
     return jsonify({'bookmarks': bookmarks}), 201
       
 @server.route('/avaaz/api/v1.0/bookmarks', methods=['POST'])
@@ -102,7 +45,8 @@ def create_bookmark():
     if not request.json :
         abort(400)
         
-    connection = get_db_connection()
+    db = Database()
+    bookmarks = []
     
     for element in request.json:      
       date = normalize_date(element['date'])
@@ -110,12 +54,9 @@ def create_bookmark():
       url = scrub_input(element['uri'])
       
       if (date != -1 and title != -1 and url != -1):
-        query = "INSERT INTO data (url, title, date_added) VALUES (%s, %s, %s)"
-        params = (url, title, date)
-        cursor = connection.cursor()
-        result = cursor.execute(query,params)
-        connection.commit()
-        cursor.close()
+        result = db.insert("INSERT INTO data (url, title, date_added) VALUES (%s, %s, %s)", (url, title, date))
+        if (result != 1):
+        	print(result)
         
         bookmark = {
             'title': title,
@@ -123,8 +64,6 @@ def create_bookmark():
             'date': date  
         }
         bookmarks.append(bookmark)
-    
-    connection.close()
     
     return jsonify({'bookmarks': bookmarks}), 201
       
