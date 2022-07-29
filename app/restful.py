f6bb076718038d048a0cf9eb4ce0fb46e782e72e
CH <gigamegs@gmail.com>
2022-07-29 11:21:34 +0200

    flask_restful refactor

--- app/restful.py
+++ app/restful.py
@@ -1,27 +1,30 @@
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
+from database import *
 
-api = Api(app)
+api = Api(server)
+db = Database()
 
-class BookmarksAPI(Resource):
+class BookmarkListAPI(Resource):
     def get(self):
-        pass
-
+        result = db.fetchall("SELECT title,url,date_added from data", "")
+        return jsonify({'bookmarks':result})
+        
     def post(self):
         pass
 
-class BookmarkAPI(Resource):
+class BookmarksAPI(Resource):
     def get(self, id):
-        pass
+        return jsonify({'bookmark':db.fetchone("SELECT id,title,url,date_added from data WHERE id = %s", id)})
 
-class BookmarkTitleAPI(Resource):
-    def get(self, id):
-        pass
+class BookmarksTitleAPI(Resource):
+    def get(self, title):
+        return jsonify({'bookmarks':db.fetchall("SELECT title,url,date_added from data where title Like %s", ("%"+title+"%"))})
 
-class BookmarkUrlAPI(Resource):
-    def get(self, id):
-        pass
-        
-api.add_resource(BookmarksAPI, '/avaaz/api/v1.0/bookmarks', endpoint = 'bookmarks')
-api.add_resource(BookmarkAPI, '/avaaz/api/v1.0/bookmarks/<int:id>', endpoint = 'bookmarks')
-api.add_resource(BookmarksTitleAPI, '/avaaz/api/v1.0/bookmarks/title/<string:bookmark_title>', endpoint = 'title')
-api.add_resource(BookmarksUrlAPI, '/avaaz/api/v1.0/bookmark/url/<string:bookmark_url>', endpoint = 'url')
+class BookmarksUrlAPI(Resource):
+    def get(self, url):
+       return jsonify({'bookmarks':db.fetchall("SELECT title,url,date_added from data where REPLACE(REPLACE(url,'http://',''),'https://','') Like %s", ("%"+url+"%"))})
+
+api.add_resource(BookmarkListAPI, '/avaaz/api/v1.0/bookmarks', endpoint = 'bookmarks')
+api.add_resource(BookmarksAPI, '/avaaz/api/v1.0/bookmark/<int:id>', endpoint = 'bookmark')
+api.add_resource(BookmarksTitleAPI, '/avaaz/api/v1.0/bookmarks/title/<string:title>', endpoint = 'title')
+api.add_resource(BookmarksUrlAPI, '/avaaz/api/v1.0/bookmarks/url/<string:url>', endpoint = 'url')
